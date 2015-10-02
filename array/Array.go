a874eed90460458588296f2803a416c49d8aec4f
nzlov <978277837@qq.com>
2015-10-02 15:37:45 +0800

    move array

--- array/Array.go
+++ array/Array.go
@@ -1,11 +1,13 @@
-package utils
+package array
 
 import (
 	"errors"
+	"sync"
 )
 
 type Array struct {
 	Items []interface{}
+	m     sync.Mutex
 }
 
 func NewArray() *Array {
@@ -15,6 +17,7 @@ func NewArray() *Array {
 func NewArrayByCap(cap int) *Array {
 	a := &Array{}
 	a.Items = make([]interface{}, 0, cap)
+	a.m = sync.Mutex{}
 	return a
 }
 
@@ -24,6 +27,7 @@ func NewArrayByArray(array *Array) *Array {
 	for i := 0; i < array.Size(); i++ {
 		a.Items[i] = array.Items[i]
 	}
+	a.m = sync.Mutex{}
 	return a
 }
 
@@ -33,6 +37,7 @@ func NewArrayByIs(is ...interface{}) *Array {
 	for i := 0; i < len(is); i++ {
 		a.Items[i] = is[i]
 	}
+	a.m = sync.Mutex{}
 	return a
 }
 func NewArrayByArrayE(array []interface{}, start, count int) *Array {
@@ -41,11 +46,14 @@ func NewArrayByArrayE(array []interface{}, start, count int) *Array {
 	for i := 0; i < count; i++ {
 		a.Items[i] = array[i+start]
 	}
+	a.m = sync.Mutex{}
 	return a
 }
 
 func (this *Array) Add(v interface{}) {
+	this.m.Lock()
 	this.Items = append(this.Items, v)
+	this.m.Unlock()
 
 }
 
@@ -66,27 +74,36 @@ func (this *Array) AddAllIs(is []interface{}) {
 }
 
 func (this *Array) AddAllIsByPos(is []interface{}, offset, length int) {
+	this.m.Lock()
 	for i := 0; i < length; i++ {
 		this.Items = append(this.Items, is[i+offset])
 	}
+	this.m.Unlock()
 }
 
 func (this *Array) Size() int {
-	return len(this.Items)
+	l := len(this.Items)
+	return l
 }
 
 func (this *Array) Get(index int) (interface{}, error) {
 	if this.Size() <= index {
 		return nil, errors.New("IndexOutOfBounds:" + string(this.Size()) + "<=" + string(index))
 	}
-	return this.Items[index], nil
+
+	this.m.Lock()
+	i := this.Items[index]
+	this.m.Unlock()
+	return i, nil
 }
 
 func (this *Array) Set(index int, v interface{}) error {
 	if this.Size() <= index {
 		return errors.New("IndexOutOfBounds:" + string(this.Size()) + "<=" + string(index))
 	}
+	this.m.Lock()
 	this.Items[index] = v
+	this.m.Unlock()
 	return nil
 }
 
@@ -94,6 +111,7 @@ func (this *Array) Insert(index int, v interface{}) error {
 	if this.Size() <= index {
 		return errors.New("IndexOutOfBounds:" + string(this.Size()) + "<=" + string(index))
 	}
+	this.m.Lock()
 	oldA := make([]interface{}, this.Size())
 	copy(oldA, this.Items)
 	this.Items = make([]interface{}, this.Size()+1)
@@ -104,6 +122,7 @@ func (this *Array) Insert(index int, v interface{}) error {
 	for i := index + 1; i < len(oldA)+1; i++ {
 		this.Items[i] = oldA[i-1]
 	}
+	this.m.Unlock()
 	return nil
 }
 
@@ -114,9 +133,11 @@ func (this *Array) Swap(first, second int) error {
 	if second >= this.Size() {
 		return errors.New("IndexOutOfBounds:" + string(this.Size()) + "<=" + string(second))
 	}
+	this.m.Lock()
 	a := this.Items[first]
 	this.Items[first] = this.Items[second]
 	this.Items[second] = a
+	this.m.Unlock()
 	return nil
 }
 
@@ -153,6 +174,7 @@ func (this *Array) RemoveIndex(index int) (interface{}, error) {
 	if index >= this.Size() {
 		return nil, errors.New("IndexOutOfBounds:" + string(this.Size()) + "<=" + string(index))
 	}
+	this.m.Lock()
 	v := this.Items[index]
 	oldA := make([]interface{}, this.Size())
 	copy(oldA, this.Items)
@@ -163,6 +185,7 @@ func (this *Array) RemoveIndex(index int) (interface{}, error) {
 	for i := index + 1; i < len(oldA); i++ {
 		this.Items[i-1] = oldA[i]
 	}
+	this.m.Unlock()
 	return v, nil
 }
 
